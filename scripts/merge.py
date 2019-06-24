116b5a0979ab5e77a04a6bbab23b2fe9a86ee64d
Rose Orenbuch <rose.orenbuch@columbia.edu>
2019-06-24 20:43:59 +0000

    Changed merge output to table and added gene count table.

--- scripts/merge.py
+++ scripts/merge.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #-------------------------------------------------------------------------------
-#   merge.py: merges genotype files into a single json or table.
+#   merge.py: merges genotype files into a single table.
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
@@ -25,7 +25,9 @@
 import os
 import json
 import argparse
+import pandas as pd
 
+from collections import defaultdict
 from argparse import RawTextHelpFormatter
 
 from arcas_utilities import check_path
@@ -39,20 +41,21 @@ def get_paths(indir):
     '''Get all file paths that match arcasHLA output.'''
     partial_files = []
     genotype_files = []
+    gene_count_files = []
     
     for file in os.listdir(indir):
         if file.endswith('.partial_genotype.json'):
             partial_files.append(file)
         elif file.endswith('.genotype.json'):
             genotype_files.append(file)
+        elif file.endswith('.genes.json'):
+            gene_count_files.append(file)
             
-    return genotype_files, partial_files 
+    return genotype_files, partial_files, gene_count_files
     
-def process_json(json_files, indir, outdir, run, suffix):
-    '''Merge genotype.json files.'''
-    file_out = [outdir]
-    if run: file_out.extend([run,'.'])
-    file_out.append(suffix)
+def process_genotype(json_files, indir, outdir, run, suffix):
+    '''Merge genotype.json files into single tsv.'''
+    file_out = ''.join([outdir, run, suffix])
     
     genotypes = dict()
     for file in json_files:
@@ -61,9 +64,45 @@ def process_json(json_files, indir, outdir, run, suffix):
 
         with open(file_path,'r') as file:
             genotypes[sample] = json.load(file)
+        
+    genotypes_ = defaultdict(dict)
+    for sample, genotype in genotypes.items():
+        for gene, alleles in genotype.items():
+            if len(alleles) == 2:
+                genotypes_[sample][gene + '1'] = alleles[0]
+                genotypes_[sample][gene + '2'] = alleles[1]
+            else:
+                genotypes_[sample][gene + '1'] = alleles[0]
+                genotypes_[sample][gene + '2'] = alleles[0]
+
+    pd.DataFrame(genotypes_).T.rename_axis('subject').to_csv(file_out + '.tsv', sep = '\t')
+    
+def process_count(count_files, indir, outdir, run, suffix):
+    '''Merge gene counts into single tsv.'''
+    file_out = ''.join([outdir, run, suffix])
+    
+    all_counts = []
+    for file in count_files:
+        sample = file.split('.')[0]
+        file_path = indir + file
+
+        with open(file_path, 'r') as file:
+            lines = file.read()
+
+        lines = lines.split('-'*80)[2].split('\n')
+        counts = {'subject':sample}
+        for line in lines:
+            if line.startswith('[alignment] Processed '):
+                _,_,counts['total_count'],_,counts['aligned_count'],_,_,_,_ = line.split()
+            elif line.endswith(' reads mapped to a single HLA gene'):
+                counts['single_count'] = line.split()[1]
+            elif line.startswith('\t\tHLA-'):
+                line = line.split()
+                gene = line[0].split('-')[1]
+                counts[gene + '_read_count'] = line[2]
+        all_counts.append(counts)
             
-    with open(''.join(file_out), 'w') as file:
-        json.dump(genotypes, file)
+    pd.DataFrame(all_counts).set_index('subject').to_csv(file_out, sep = '\t')
     
 
 if __name__ == '__main__':
@@ -106,22 +145,31 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
+    if args.run: args.run += '.'
+    
     indir, outdir = [check_path(path) for path in [args.indir, args.outdir]]
     
-    genotype_files, partial_files = get_paths(indir)
+    genotype_files, partial_files, gene_count_files = get_paths(indir)
     
     if genotype_files:
-        process_json(genotype_files, 
-                     indir, 
-                     outdir, 
-                     args.run, 
-                     'genotypes.json')
+        process_genotype(genotype_files, 
+                         indir, 
+                         outdir, 
+                         args.run, 
+                         'genotypes')
         
     if partial_files:
-        process_json(partial_files, 
-                     indir, 
-                     outdir,  
-                     args.run, 
-                     'partial_genotypes.json')
+        process_genotype(partial_files, 
+                         indir, 
+                         outdir,  
+                         args.run, 
+                         'partial_genotypes')
+        
+    if gene_count_files:
+        process_count(gene_count_files, 
+                      indir, 
+                      outdir,  
+                      args.run, 
+                      'genes')
         
 #-------------------------------------------------------------------------------
