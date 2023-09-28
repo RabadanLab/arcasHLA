597904b4e31bc765ceaa52aafd30dfab7049998e
Alejandro Buendia <alb2281@columbia.edu>
2023-09-28 20:33:52 +0000

    Test container fix

--- test/test_arcas_hla.py
+++ test/test_arcas_hla.py
@@ -34,7 +34,7 @@ def test_whole_allele_typing():
         "C": ["C*08:01:01", "C*01:02:01"],
         "DPB1": ["DPB1*14:01:01", "DPB1*02:01:02"],
         "DQA1": ["DQA1*02:01:01", "DQA1*05:03"],
-        "DQB1": ["DQB1*02:02:01", "DQB1*06:09:01"],
+        "DQB1": ["DQB1*06:09:01", "DQB1*02:02:01"],
         "DRB1": ["DRB1*10:01:01", "DRB1*14:02:01"]
     }
     with open(output_file, "r") as f:
@@ -48,13 +48,13 @@ def test_partial_allele_typing():
     subprocess.run(partial_typing_cmd.split(), shell=True)
 
     output_file = "./test/output/test.partial_genotype.json"
-    expected_output_json = {
+    expected_output = {
         "A": ["A*01:01:01", "A*03:01:01"],
         "B": ["B*07:02:01", "B*39:39:01"],
         "C": ["C*08:01:01", "C*01:02:01"],
         "DPB1": ["DPB1*14:01:01", "DPB1*02:01:02"],
         "DQA1": ["DQA1*02:01:01", "DQA1*05:03"],
-        "DQB1": ["DQB1*06:04:01", "DQB1*02:02:01"],
+        "DQB1": ["DQB1*02:02:01", "DQB1*06:04:01"],
         "DRB1": ["DRB1*03:02:01", "DRB1*14:02:01"]
     }
     with open(output_file, "r") as f:
